d8f0dd30c365f188ff16e129afaabba8037c0768
Giovanni Dejan <giovanni.dejan@binus.ac.id>
2022-06-17 23:34:01 +0700

    fix: refactor

--- src/socket.ts
+++ src/socket.ts
@@ -16,6 +16,11 @@ interface Point {
   col: number;
 }
 
+function broadcast(socket: Socket, event: string, payload: unknown) {
+  socket.emit(event, payload);
+  socket.broadcast.emit(event, payload);
+}
+
 function onPlayerJoined(socket: Socket) {
   let character: Character;
   if (charToPlayerMap.has(Character.X)) {
@@ -30,54 +35,77 @@ function onPlayerJoined(socket: Socket) {
   socket.emit(Event.PLAYER_JOINED, { socketId: socket.id, character });
 
   if (playerToCharMap.size === 2) {
-    socket.emit(Event.GAME_STARTED);
-    socket.emit(Event.PLAYER_MOVES, {player: Character.X});
+    broadcast(socket, Event.GAME_STARTED, {});
+    broadcast(socket, Event.PLAYER_MOVES, { player: Character.X });
   }
 }
 
-// TODO: refactor
-function isWinning(player: string): boolean {
-  for(let c = 0; c < 3; c++) {
+function isWinningVertically(player: string): boolean {
+  for (let c = 0; c < 3; c++) {
     let count = 0;
-    for(let r = 0; r < 3; r++) {
-      if(board[r][c] === player) {
+    for (let r = 0; r < 3; r++) {
+      if (board[r][c] === player) {
         count++;
       }
     }
-    if(count == 3) {
+    if (count == 3) {
       return true;
     }
   }
 
-  for(let r = 0; r < 3; r++) {
+  return false;
+}
+
+function isWinningHorizontally(player: string): boolean {
+  for (let r = 0; r < 3; r++) {
     let count = 0;
-    for(let c = 0; c < 3; c++) {
-      if(board[r][c] === player) {
+    for (let c = 0; c < 3; c++) {
+      if (board[r][c] === player) {
         count++;
       }
     }
-    if(count === 3) {
+    if (count === 3) {
       return true;
     }
   }
 
+  return false;
+}
+
+function isWinningDiagonally(player: string): boolean {
   let count = 0;
-  for(let i = 0; i < 3; i++) {
-    if(board[i][i] === player) {
+  for (let i = 0; i < 3; i++) {
+    if (board[i][i] === player) {
       count++;
     }
   }
-  if(count === 3) {
+  if (count === 3) {
     return true;
   }
 
   count = 0;
-  for(let i = 0, j = 2; i < 3; i++, j--) {
-    if(board[i][j] === player) {
+  for (let i = 0, j = 2; i < 3; i++, j--) {
+    if (board[i][j] === player) {
       count++;
     }
   }
-  if(count === 3) {
+  if (count === 3) {
+    return true;
+  }
+
+  return false;
+}
+
+function isWinning(player: string): boolean {
+  if (isWinningVertically(player)) {
+    return true;
+  }
+
+  if (isWinningHorizontally(player)) {
+    return true;
+  }
+
+  if (isWinningDiagonally(player)) {
     return true;
   }
 
@@ -87,41 +115,43 @@ function isWinning(player: string): boolean {
 function onPlayerMoves(socket: Socket, point: Point) {
   const player: string = playerToCharMap.get(socket.id)!;
   if (board[point.row][point.col] !== "") {
-    socket.emit(Event.INVALID_MOVE, { player });
+    broadcast(socket, Event.INVALID_MOVE, { player });
     return;
   }
 
   board[point.row][point.col] !== player;
 
-  if(isWinning(player)) {
-    socket.emit(Event.PLAYER_WINS, {player});
+  if (isWinning(player)) {
+    broadcast(socket, Event.PLAYER_WINS, { player });
     return;
   }
 
   // draw
   let emptySlotCount = 0;
-  for(let r = 0; r < 3; r++) {
-    for(let c = 0; c < 3; c++) {
-      if(board[r][c] === "") {
+  for (let r = 0; r < 3; r++) {
+    for (let c = 0; c < 3; c++) {
+      if (board[r][c] === "") {
         emptySlotCount++;
       }
     }
   }
-  if(emptySlotCount === 0) {
+  if (emptySlotCount === 0) {
     socket.emit(Event.DRAW);
   }
 
   let nextPlayer: string;
-  if(player == Character.X) {
+  if (player == Character.X) {
     nextPlayer = Character.O;
   } else {
     nextPlayer = Character.X;
   }
 
-  socket.emit(Event.PLAYER_MOVES, {player: nextPlayer});
+  broadcast(socket, Event.PLAYER_MOVES, { player: nextPlayer });
 }
 
 export default function handle(socket: Socket) {
   socket.on(Event.ON_PLAYER_JOINED, () => onPlayerJoined(socket));
-  socket.on(Event.ON_PLAYER_MOVES, (point: Point) => onPlayerMoves(socket, point));
+  socket.on(Event.ON_PLAYER_MOVES, (point: Point) =>
+    onPlayerMoves(socket, point)
+  );
 }
