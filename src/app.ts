252f5855671347cf631273133a384401d51726d3
Giovanni Dejan <giovanni.dejan@binus.ac.id>
2022-07-23 18:27:00 +0700

    fix: set as healty and set ready

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,49 @@
 import { Server } from "socket.io";
 import handle from "./socket";
 
+function setAsHealthy() {
+  const myHeaders = new Headers();
+  myHeaders.append("Content-Type", "application/json");
+
+  const raw = JSON.stringify({});
+
+  const requestOptions: RequestInit = {
+    method: "POST",
+    headers: myHeaders,
+    body: raw,
+    redirect: "follow",
+  };
+
+  fetch(`http://localhost:${process.env.AGONES_SDK_HTTP_PORT}/ready`, requestOptions)
+    .then((response) => response.text())
+    .then((result) => console.log(result))
+    .catch((error) => console.log("error", error));
+}
+
+function setAsReady(): void {
+  const myHeaders = new Headers();
+  myHeaders.append("Content-Type", "application/json");
+
+  const raw = JSON.stringify({});
+
+  const requestOptions: RequestInit = {
+    method: "POST",
+    headers: myHeaders,
+    body: raw,
+    redirect: "follow",
+  };
+
+  fetch(
+    `http://localhost:${process.env.AGONES_SDK_HTTP_PORT}/ready`,
+    requestOptions
+  )
+    .then((response) => response.text())
+    .then((result) => console.log(result))
+    .catch((error) => console.log("error", error));
+}
+
 const io = new Server({
-  "transports": ["websocket"]
+  transports: ["websocket"],
 });
 
 io.on("connection", (socket) => {
@@ -12,6 +53,9 @@ io.on("connection", (socket) => {
   socket.on("disconnected", () => {
     console.log(`socket ${socket.id} is disconnected`);
   });
+
+  setAsHealthy();
+  setAsReady();
 });
 
 io.listen(4000);
